7f65a3f3f88c5528acbc537f8d7ebf608cf5eb64
tomy9729@naver.com <tomy9729@naver.com>
2022-01-19 18:06:37 +0900

    feature_F04-01,02_FE_이거사자_김현수

---
+++ Front-end/woorisaza/src/views/ThisSaza/ArticleDetail.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        
+        {{articleNo}}
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Articledetail',
+    data() {
+        return {
+            articleNo : this.$route.params.articleno,
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- Front-end/woorisaza/src/views/ThisSaza/ThisSaza.vue
+++ Front-end/woorisaza/src/views/ThisSaza/ThisSaza.vue
@@ -1,45 +1,76 @@
 <template>
-    <div class="mt-3" style="background-color : #FFFDF2; width:100%;height:87vh">
-        <div class="container">
-            <div class="pt-5">
+    <div class="mt-3" style="background-color : #FFFDF2; width:100%;height:100%">
+            <div class="pt-5 pb-5">
+                <!-- pc버전 -->
                 <b-container >
                     <b-row class="">
                         <b-col></b-col>
                         <b-col cols="7">
-                            <Board></Board>
+                            <div :class="{box:true}" style="overflow-y:auto;">
+                                <div class="p-5">
+                                    <component :is="selectComponent"></component>
+                                </div>
+                            </div>
                         </b-col>
                         <b-col></b-col>
                     </b-row>
                 </b-container>
+                <!-- 모바일버전 -->
+                <!-- <Board></Board> -->
             </div>
-            
-        </div>
     </div>
 </template>
 
 <script>
 import Board from "@/views/ThisSaza/Board.vue";
+import ArticleDetail from "@/views/ThisSaza/ArticleDetail.vue";
 export default {
     name: 'Thissaza',
     components : {
         Board,
+        ArticleDetail,
     },
     data() {
         return {
-            
+            selectComponent : "Board",
         };
     },
-
+    computed:{
+        getArticlNo(){
+            // return this.$store.getters.getArticleNo;
+            return this.$route.params.articleno;
+        }
+    },
+    watch:{
+        getArticlNo(val){
+            if(val==null){
+                this.selectComponent = "Board";
+            }
+            else{
+                this.selectComponent = "ArticleDetail";
+            }
+        },
+    },
     mounted() {
-        
     },
 
     methods: {
-        
     },
 };
 </script>
 
-<style lang="scss" scoped>
+<style >
+    .box{
+        background-color:white; 
+        box-shadow: 0px 0px 5px 0.1px grey; 
+        border-radius: 0.5em;
+        width:100%;
+        height:100%;
+        -ms-overflow-style: none; /* IE and Edge */
+        scrollbar-width: none; /* Firefox */
+    }
+    .box::-webkit-scrollbar {
+        display: none; /* Chrome, Safari, Opera*/
+    }
 
 </style>
