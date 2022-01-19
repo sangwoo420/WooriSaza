be0d2debe96f4a6fd15789c5dedf58502ddddc0a
sangwoo420 <sangwoo420@gmail.com>
2022-01-19 14:09:07 +0900

    F_DB_PSW

---
+++ Back-end/src/main/java/project/woori_saza/model/domain/Article.java
@@ -0,0 +1,52 @@
+package project.woori_saza.model.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.bytebuddy.asm.Advice;
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+
+    @Id
+    @Column(name = "article_id")
+    private long id;
+
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private String link;
+    private LocalDateTime deadline;
+    private int totalPrice;
+    private int totalCount;
+    private int recruitCount;
+    private boolean isDone;
+    private List<String> pic;
+
+    //FK
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "party_id")
+    private Party party;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
