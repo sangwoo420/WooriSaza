83c87360a3a00af37361332229d87f704bdaf2b5
sangwoo420 <sangwoo420@gmail.com>
2022-01-21 13:06:54 +0900

    DB_PSW

--- Back-end/src/main/java/project/woori_saza/model/repo/CommentRepo.java
+++ Back-end/src/main/java/project/woori_saza/model/repo/CommentRepo.java
@@ -2,6 +2,11 @@ package project.woori_saza.model.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.woori_saza.model.domain.Comment;
+import project.woori_saza.model.domain.UserProfile;
+
+import java.util.List;
 
 public interface CommentRepo extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByUserProfile(UserProfile userProfile);
 }
--- Back-end/src/main/java/project/woori_saza/model/repo/UserAuthRepo.java
+++ Back-end/src/main/java/project/woori_saza/model/repo/UserAuthRepo.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.woori_saza.model.domain.UserAuth;
 
 public interface UserAuthRepo extends JpaRepository<UserAuth,String> {
+
+
 }
