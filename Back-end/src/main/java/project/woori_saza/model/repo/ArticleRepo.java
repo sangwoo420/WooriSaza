d2f988267a5a87877337e35e527ac6e66d11bf79
yunjiyeong <mer.toonine@gmail.com>
2022-01-21 11:44:24 +0900

    userprofile_delete

--- Back-end/src/main/java/project/woori_saza/model/repo/ArticleRepo.java
+++ Back-end/src/main/java/project/woori_saza/model/repo/ArticleRepo.java
@@ -2,8 +2,12 @@ package project.woori_saza.model.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.woori_saza.model.domain.Article;
+import project.woori_saza.model.domain.UserProfile;
+
+import java.util.List;
 
 public interface ArticleRepo extends JpaRepository<Article, Long> {
 
+    List<Article> findByUserProfile(UserProfile userProfile);
 
 }
