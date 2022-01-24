de6cf3d8e9ad50f980150a47cb62f1822281e129
sangwoo420 <sangwoo420@gmail.com>
2022-01-24 10:22:04 +0900

    enable swagger

--- Back-end/src/main/java/project/woori_saza/configuration/SwaggerConfig.java
+++ Back-end/src/main/java/project/woori_saza/configuration/SwaggerConfig.java
@@ -1,32 +1,33 @@
-//package project.woori_saza.configuration;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//    @Bean
-//    public Docket postsApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .groupName("woori_saza")
-//                .apiInfo(apiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("project.woori_saza.model.controller"))
-//                .paths(PathSelectors.ant("/**"))
-//                .build();
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder().title("woorisaza API")
-//                .description("woorisaza API Reference for Developers")
-//                .version("1.0").build();
-//    }
-//}
+package project.woori_saza.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+// http://localhost:8080/swagger-ui/index.html
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public Docket postsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("woori_saza")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("project.woori_saza.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("woorisaza API")
+                .description("woorisaza API Reference for Developers")
+                .version("1.0").build();
+    }
+}
