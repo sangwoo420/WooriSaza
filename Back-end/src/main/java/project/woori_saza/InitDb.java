8214a28cb81b9a20f3eb60f423f0277571d00493
yunjiyeong <mer.toonine@gmail.com>
2022-01-25 15:13:02 +0900

    modified init db

--- Back-end/src/main/java/project/woori_saza/InitDb.java
+++ Back-end/src/main/java/project/woori_saza/InitDb.java
@@ -92,16 +92,6 @@ public class InitDb {
             Comment comment3 = new Comment(3L, "content3", LocalDateTime.now(), articleRepo.getById(1L), user);
             commentRepo.save(comment3);
 
-            Comment comment = new Comment(1L, "content", LocalDateTime.now(), article, user);
-            commentRepo.save(comment);
-//
-//
-//            Comment comment2 = new Comment(2L, "content2", LocalDateTime.now(), article, user2);
-//            commentRepo.save(comment2);
-//
-//            Comment comment3 = new Comment(3L, "content3", LocalDateTime.now(), article, user);
-//            commentRepo.save(comment3);
-//
 //            Zzim zzim=new Zzim(user,article);
 //            zzimRepo.save(zzim);
 
