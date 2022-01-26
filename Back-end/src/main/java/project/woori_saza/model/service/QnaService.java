87bbc3a19ad8922a70107cbfbf039f4054f2f3f2
yunjiyeong <mer.toonine@gmail.com>
2022-01-26 10:23:39 +0900

    bug problems solved and changed method name

--- Back-end/src/main/java/project/woori_saza/model/service/QnaService.java
+++ Back-end/src/main/java/project/woori_saza/model/service/QnaService.java
@@ -1,5 +1,6 @@
 package project.woori_saza.model.service;
 
+import project.woori_saza.model.domain.Qna;
 import project.woori_saza.model.dto.QnaDto;
 
 import java.util.List;
@@ -8,6 +9,7 @@ public interface QnaService {
 
     List<QnaDto> getMyQnaList(String profileId);
     List<QnaDto> getQnaList();
+    QnaDto getQnaDetail(Long qnaId);
     void insertQna(QnaDto qnaDto);
     void updateQna(QnaDto qnaDto);
     void deleteQna(Long qnaId);
