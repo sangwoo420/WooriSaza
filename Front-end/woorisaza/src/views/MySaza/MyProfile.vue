ad5cb633e3c808e43122c0e07e7f10c06c8d42c3
tomy9729@naver.com <tomy9729@naver.com>
2022-02-07 17:50:12 +0900

    kakao

--- Front-end/woorisaza/src/views/MySaza/MyProfile.vue
+++ Front-end/woorisaza/src/views/MySaza/MyProfile.vue
@@ -41,7 +41,7 @@ export default {
       method : "get",
       url : "user/"+this.id,
     }).then(({data})=>{
-      // console.log(data)
+      console.log(data)
       this.profile = data.profile;
     })
   },
