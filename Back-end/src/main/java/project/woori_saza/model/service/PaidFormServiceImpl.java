fa697a4d4e4762c436ab2e533fc0214f265933e1
sojin0706 <sojin0706@naver.com>
2022-02-07 19:55:42 +0900

    F15_BE_PSJ_사진 업로드

--- Back-end/src/main/java/project/woori_saza/model/service/PaidFormServiceImpl.java
+++ Back-end/src/main/java/project/woori_saza/model/service/PaidFormServiceImpl.java
@@ -2,8 +2,16 @@ package project.woori_saza.model.service;
 
 import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import project.woori_saza.model.domain.PaidForm;
 import project.woori_saza.model.dto.PaidFormRequestDto;
@@ -24,6 +32,9 @@ public class PaidFormServiceImpl implements PaidFormService{
     @Autowired
     PartyRepo partyRepo;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @Override
     public PaidFormResponseDto getPaidForm(Long paidFormId) {
         PaidForm paidForm = paidFormRepo.getById(paidFormId);
@@ -33,25 +44,56 @@ public class PaidFormServiceImpl implements PaidFormService{
 
     @Override
     @Transactional
-    public void insertPaidForm(PaidFormRequestDto paidFormRequestDto,MultipartFile multipartFile) throws Exception {
-
-            File file=new File("");
-
-            String originFilename = multipartFile.getOriginalFilename();
-            String extension = originFilename.substring(originFilename.length()-3);
-
-            if(!(extension.equals("jpg") || extension.equals("png"))){
-                throw new FileUploadException("파일 확장자가 jpg나 png가 아닙니다.");
-            }
-            //파일이름 랜덤으로 만들기
-            String saveFileName = UUID.randomUUID().toString() + originFilename.substring(originFilename.lastIndexOf(".")); //랜덤이름+확장자
-            System.out.println("랜덤이름 출력"+saveFileName);
+    public void insertPaidForm(PaidFormRequestDto paidFormRequestDto) {
 
             PaidForm paidForm = paidFormRequestDto.toEntity();
-            paidForm.setPic(saveFileName);
             paidForm.setParty(partyRepo.getById(paidFormRequestDto.getParty()));
             paidFormRepo.save(paidForm);
+
     }
+
+    @Override
+    public String upload(MultipartFile uploadFile) throws Exception {
+
+        // 파일 정보
+        String originFilename = uploadFile.getOriginalFilename(); //파일이름
+        String extension = originFilename.substring(originFilename.length()-3); //확장자
+
+        // 사진인지 체크
+        if(!(extension.equals("jpg") || extension.equals("png")|| extension.equals("PNG")|| extension.equals("JPG"))){
+            throw new FileUploadException("파일 확장자가 jpg나 png가 아닙니다.");
+        }
+        //파일이름 랜덤으로 만들기
+        String url="/paidform/";
+        String saveFileName =UUID.randomUUID().toString() + originFilename.substring(originFilename.lastIndexOf(".")); //랜덤이름+확장자
+        String saveFileName2=url+saveFileName;
+        System.out.println("랜덤이름 출력할래 "+saveFileName2);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        //파일 바이트
+        ByteArrayResource fileAsResource = new ByteArrayResource(uploadFile.getBytes()){
+            @Override
+            public String getFilename() {
+                return saveFileName2;
+            }
+        };
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("uploadFile", fileAsResource); //파일 바이트 저장
+        body.add("parentPath","paidform");
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        String serverUrl = "http://i6c102.p.ssafy.io:3000/upload";
+
+        ResponseEntity<String> response = restTemplate
+                .postForEntity(serverUrl, requestEntity, String.class);
+
+        return "http://i6c102.p.ssafy.io/images"+saveFileName2;
+
+    }
+
 
     @Override
     @Transactional
