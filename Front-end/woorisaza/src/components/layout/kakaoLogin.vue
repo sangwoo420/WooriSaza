9c4c8d72fe0c56fe6c5ccba9b0b644d1eea57bd3
tomy9729@naver.com <tomy9729@naver.com>
2022-02-14 09:56:07 +0900

    url

--- Front-end/woorisaza/src/components/layout/kakaoLogin.vue
+++ Front-end/woorisaza/src/components/layout/kakaoLogin.vue
@@ -1,35 +1,29 @@
 <template>
-    <div>
-        <img src="@/assets/kakaoLogin.png" style="cursor:pointer" @click="loginWithKakao">
-    </div>
+  <div>
+    <img src="@/assets/kakaoLogin.png" style="cursor: pointer" @click="loginWithKakao" />
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Kakaologin',
-    
-    data() {
-        return {
-            
-        };
-    },
+  name: "Kakaologin",
 
-    mounted() {
-        
-    },
+  data() {
+    return {};
+  },
 
-    methods: {
-        loginWithKakao() {
-            const params = {
-                redirectUri: "http://localhost:8081/",
-                // redirectUri: "http://i6c102.p.ssafy.io/",
-            };
-            window.Kakao.Auth.authorize(params);
-        },
+  mounted() {},
+
+  methods: {
+    loginWithKakao() {
+      const params = {
+        // redirectUri: "http://localhost:8081/",
+        redirectUri: "http://i6c102.p.ssafy.io/",
+      };
+      window.Kakao.Auth.authorize(params);
     },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
