c661a014ecc9017a716c016afa7c16d2eb4bed12
sangwoo420 <sangwoo420@gmail.com>
2022-02-14 15:52:52 +0900

    fixed article list

--- Back-end/src/main/java/project/woori_saza/model/domain/UserProfile.java
+++ Back-end/src/main/java/project/woori_saza/model/domain/UserProfile.java
@@ -81,7 +81,7 @@ public class UserProfile implements Serializable {
     @OneToMany(mappedBy = "userProfile", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Zzim> zzims = new ArrayList<>();
 
-    @OneToMany(mappedBy = "userProfile")
+    @OneToMany(mappedBy = "userProfile", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<ChatRoomJoin> chatRoomJoinList = new ArrayList<>();
 
 
